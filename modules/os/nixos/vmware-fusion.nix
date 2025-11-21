0482f960d4893b876ddb0093d98874b9bac88518
Rex Liu <hi@r3x.sh>
2025-11-20 23:00:22 -0500

    Fix VMware guest option to use virtualisation.vmware

--- modules/os/nixos/vmware-fusion.nix
+++ modules/os/nixos/vmware-fusion.nix
@@ -1,13 +1,9 @@
-{ lib, pkgs, config, ... }:
+{ lib, ... }:
 {
   # Tuning for VMware Fusion guests on Apple Silicon: prefer native VMware tools.
   boot.loader.systemd-boot.consoleMode = lib.mkDefault "0";
 
   # Pick VMware guest tools over qemu guest agent.
   services.qemuGuest.enable = lib.mkDefault false;
-  services.vmwareGuest.enable = lib.mkDefault true;
-
-  environment.systemPackages = lib.mkIf config.services.vmwareGuest.enable [
-    pkgs.open-vm-tools
-  ];
+  virtualisation.vmware.guest.enable = lib.mkDefault true;
 }
