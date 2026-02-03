1aa0c7b7e04e3a86c42c8cd5a2fcd0289f6dd129
Rex Liu <hi@r3x.sh>
2026-02-03 02:06:27 -0500

    .

--- modules/ui.nix
+++ modules/ui.nix
@@ -23,7 +23,7 @@
       "autohide-delay" = 0.0;
       "autohide-time-modifier" = 0.21;
       persistent-apps = [
-        "/Applications/Helium.app" 
+        "/Applications/Brave Browser.app" 
         "/Applications/Ghostty.app"
       ];
     };
